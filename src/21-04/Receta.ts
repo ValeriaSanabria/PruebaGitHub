8b4f840b427d3c7c0a8357f7fd4d8d08d001c61b
ValeriaSanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-04-21 21:40:37 -0300

    ejercicios

---
+++ src/21-04/Receta.ts
@@ -0,0 +1,11 @@
+/* 
+  Agregar cantidades de los ingredientes
+*/
+
+console.log("------Ingredientes para Tiramisú------");
+console.log("Queso crema");
+console.log("Crema de leche");
+console.log("Huevos");
+console.log("Café");
+console.log("azúcar");
+console.log("Vainillas");
